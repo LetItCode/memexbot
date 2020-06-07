bd7593c8e893da28cfa20582168195d86769f667
Josh Dies <LetItCode@users.noreply.github.com>
2020-06-08 01:35:38 +0300

    improove dialog

--- src/bot/widgets/settings.js
+++ src/bot/widgets/settings.js
@@ -1,4 +1,4 @@
-const { Markup } = require('telegraf')
+const { Extra, safePassThru } = require('telegraf')
 const { Widget } = require('telegraf-widget')
 
 const Pages = {
@@ -8,15 +8,30 @@ const Pages = {
 
 const settingsWidget = new Widget('settings', 'auth')
 
-settingsWidget.on(Pages.AUTH, ctx => {
-  const { reply, editMessageText, answerCbQuery, widget, i18n } = ctx
+settingsWidget.on(Pages.AUTH, async ctx => {
+  const { from, reply, editMessageText, answerCbQuery, widget, i18n, db, cw } = ctx
+
+  const Grants = { ...cw.Operations, AUTH: 'auth' }
 
   const page = widget.query && widget.query.page
-  const text = 'auth'
-  const extra = generateKeyboard(i18n, page)
 
-  answerCbQuery()
-  return widget.data ? editMessageText(text, extra) : reply(text, extra)
+  const user = await db.User.findOne({ telegramId: from.id })
+  if (!user) return safePassThru()
+  const { grants } = user
+
+  console.log(grants.includes(Grants.PROFILE))
+  let text = i18n.t('settings.auth.title') + '\n\n'
+  text += i18n.t('settings.auth.text') + '\n\n'
+  if (grants.includes(Grants.AUTH)) {
+    text += i18n.t(`settings.auth.profile.${grants.includes(Grants.PROFILE) ? 'off' : 'on'}`) + '\n'
+    text += i18n.t(`settings.auth.stock.${grants.includes(Grants.STOCK) ? 'off' : 'on'}`)
+  } else {
+    text += i18n.t('settings.auth.auth')
+  }
+
+  const extra = generateKeyboard(i18n, page).HTML()
+
+  return widget.data ? editMessageText(text, extra).catch(() => answerCbQuery()) : reply(text, extra)
 })
 
 settingsWidget.on(Pages.LANGUAGE, ctx => {
@@ -26,8 +41,7 @@ settingsWidget.on(Pages.LANGUAGE, ctx => {
   const text = 'lang'
   const extra = generateKeyboard(i18n, page)
 
-  answerCbQuery()
-  return widget.data ? editMessageText(text, extra) : reply(text, extra)
+  return widget.data ? editMessageText(text, extra).catch(() => answerCbQuery()) : reply(text, extra)
 })
 
 module.exports = settingsWidget
@@ -37,7 +51,7 @@ function generateKeyboard (i18n, currentPage = Pages.AUTH) {
     generateButton(i18n.t('buttons.auth'), Pages.AUTH, currentPage),
     generateButton(i18n.t('buttons.language'), Pages.LANGUAGE, currentPage)
   ]
-  return Markup.inlineKeyboard(buttons).extra()
+  return Extra.markup(m => m.inlineKeyboard(buttons))
 }
 
 function generateButton (title, page, currentPage) {
