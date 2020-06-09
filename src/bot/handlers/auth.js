8e94f30f6711a42a8f484fe495a8970df416b7f3
Josh Dies <LetItCode@users.noreply.github.com>
2020-06-09 16:47:11 +0300

    less code & add revoke

--- src/bot/handlers/auth.js
+++ src/bot/handlers/auth.js
@@ -5,33 +5,24 @@ module.exports = async ctx => {
   const { match, from, replyWithHTML, session, i18n, db, cw } = ctx
   const { cmd, param = 'auth' } = match.groups
 
-  const Grants = { ...cw.Operations, AUTH: 'auth' }
+  const Grants = { ...cw.Operations, AUTH: 'Auth' }
+  let authType
+  switch (param) {
+    case 'auth': authType = Grants.AUTH; break // prettier-ignore
+    case 'profile': authType = Grants.PROFILE; break // prettier-ignore
+    case 'stock': authType = Grants.STOCK; break // prettier-ignore
+    case 'gear': authType = Grants.GEAR; break // prettier-ignore
+  }
 
   const user = await db.User.findOne({ telegramId: from.id })
   if (!user) return safePassThru()
 
   if (cmd === 'auth') {
-    let res
-    let authType
     try {
-      switch (param) {
-        case 'auth':
-          authType = Grants.AUTH
-          res = await cw.createAuthCode(from.id)
-          break
-        case 'profile':
-          authType = Grants.PROFILE
-          res = await cw.authAdditionalOperation(user.token, Grants.PROFILE, from.id)
-          break
-        case 'stock':
-          authType = Grants.STOCK
-          res = await cw.authAdditionalOperation(user.token, Grants.STOCK, from.id)
-          break
-        case 'gear':
-          authType = Grants.GEAR
-          res = await cw.authAdditionalOperation(user.token, Grants.GEAR, from.id)
-          break
-      }
+      const res =
+        authType === Grants.AUTH
+          ? await cw.createAuthCode(from.id)
+          : await cw.authAdditionalOperation(user.token, authType, from.id)
 
       session.auth = { pending: true, type: authType, uuid: res.uuid }
 
@@ -40,6 +31,21 @@ module.exports = async ctx => {
       return replyWithHTML(text)
     } catch (err) {
       debug('Auth error:', err)
+    }
+  }
+
+  if (cmd === 'auth_revoke') {
+    console.log(authType)
+    try {
+      authType === Grants.AUTH
+        ? db.User.updateOne({ telegramId: from.id }, { grants: [] }).exec()
+        : db.User.updateOne({ telegramId: from.id }, { $pull: { grants: authType } }).exec()
+
+      const text = i18n.t('auth.revoke')
+
+      return replyWithHTML(text)
+    } catch (err) {
+      debug('Auth revoke error:', err)
     }
   }
 }
